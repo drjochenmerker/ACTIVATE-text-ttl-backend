bf2d5fbcaa518efddaf49aec0bd3e84e84ab5d45
alexanderreiprich <al3xz0n3@gmail.com>
2026-01-18 15:52:42 +0100

    added student/root discrimination

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -7,21 +7,34 @@ const router = Router();
 router.post('/', async (req, res) => {
 	writeToLog(logFilenames.misc, "Login attempt: ", JSON.stringify(req.headers));
 	const password = req.body.password;
-	// TODO: Differentiate between student password and root password	(instructor view)
+
 	if (password == `${process.env.ROOT_PASSWORD}`) {
 		res.status(200).json({
 			success: true,
+			role: 'root',
 			message: 'Login successful'
 		});
-		writeToLog(logFilenames.misc, "Login succeeded: ", JSON.stringify(req.headers));
+		writeToLog(logFilenames.misc, "Root login succeeded: ", JSON.stringify(req.headers));
 		return;
 	}
-	writeToLog(logFilenames.misc, "Login failed: ", JSON.stringify(req.headers));
-	res.status(401).json({
-		success: false,
-		error: 'Wrong password'
-	});
-	return;
+	else if (password == `${process.env.STUDENT_PASSWORD}`) {
+		res.status(200).json({
+			success: true,
+			role: 'student',
+			message: 'Login successful'
+		});
+		writeToLog(logFilenames.misc, "Student login succeeded: ", JSON.stringify(req.headers));
+		return;
+	}
+	else {
+		writeToLog(logFilenames.misc, "Login failed: ", JSON.stringify(req.headers));
+		res.status(401).json({
+			success: false,
+			error: 'Wrong password'
+		});
+		return;
+	}
+
 });
 
 
