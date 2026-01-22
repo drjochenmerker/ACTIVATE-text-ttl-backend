e237dabe1f924d943b45ab7e81a1513fcb2ee453
alexanderreiprich <al3xz0n3@gmail.com>
2026-01-22 17:09:51 +0100

    fix: encryption

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,14 +1,17 @@
 import { Router } from 'express';
 import { writeToLog } from '../services/utils';
 import { logFilenames } from '../data/staticContent';
+import bcrypt from 'bcrypt';
 
 const router = Router();
 
 router.post('/', async (req, res) => {
 	writeToLog(logFilenames.misc, "Login attempt: ", JSON.stringify(req.headers));
 	const password = req.body.password;
+	const rootPasswordHash = process.env.ROOT_PASSWORD;
+	const studentPasswordHash = process.env.STUDENT_PASSWORD;
 
-	if (password == `${process.env.ROOT_PASSWORD}`) {
+	if (rootPasswordHash && await bcrypt.compare(rootPasswordHash, password)) {
 		res.status(200).json({
 			success: true,
 			role: 'root',
@@ -17,7 +20,7 @@ router.post('/', async (req, res) => {
 		writeToLog(logFilenames.misc, "Root login succeeded: ", JSON.stringify(req.headers));
 		return;
 	}
-	else if (password == `${process.env.STUDENT_PASSWORD}`) {
+	else if (studentPasswordHash && await bcrypt.compare(studentPasswordHash, password)) {
 		res.status(200).json({
 			success: true,
 			role: 'student',
