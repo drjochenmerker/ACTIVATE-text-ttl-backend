59fc33013fdb942fc2344ef8e971e54b3a45b497
Jeremias Glombitza <jeremias.glombitza@stud.htwk-leipzig.de>
2026-02-01 13:31:44 +0100

    Test for failing pipeline

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
-import { writeToLog } from '../services/utils.js';
-import { logFilenames } from '../data/staticContent.js';
+import { writeToLog } from '../services/utils';
+import { logFilenames } from '../data/staticContent';
 import bcrypt from 'bcrypt';
 
 const router = Router();
